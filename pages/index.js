890c4ac2c0aaac861a2e2bbe70b956a63424b1f6
Bruno Victor <brunovictor.md@gmail.com>
2022-01-31 20:01:53 -0200

    projeto final

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,10 @@
 
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
+import React from "react";
+import {useRouter} from 'next/router';
 import appConfig from '../config.json';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -62,11 +36,12 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username ,setUsername] = React.useState('nkbruno');
+  const roteamento = useRouter();
+  
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +68,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('alguem')
+             // window.location.href = '/chat';
+              roteamento.push(`/chat?username=${username}`);
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,8 +83,18 @@ export default function PaginaInicial() {
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
-
+            {/*<input 
+              type="text"
+              
+              }}
+            />*/}
             <TextField
+              value={username}
+              onChange={function (event){
+                console.log('usuario digitou ', event.target.value);
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
